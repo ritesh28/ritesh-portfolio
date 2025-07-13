05b916284ff8de2835f3d4ecec270f1c88ddc501
Ritesh Raj <rajritesh03@gmail.com>
2025-07-13 22:38:25 +1000

    wip

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,15 +1,26 @@
+'use client';
+import { MdWork } from 'react-icons/md';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 export function ExperienceSection() {
   return (
     <section id='experience' className='bg-fuchsia-500  my-4 p-8'>
       <h3 className='text-6xl leading-24 font-semibold flex items-center after:border-b-2 after:grow after:ml-6'>Experience</h3>
-      <ul>
-        <li>
-          <p>company name</p>
-          <p>position</p>
-          <p>duration</p>
-          <p>what you did there - in one-two line</p>
-        </li>
-      </ul>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--work'
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date='2011 - present'
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<MdWork />}
+        >
+          <h3 className='vertical-timeline-element-title'>Creative Director</h3>
+          <h4 className='vertical-timeline-element-subtitle'>Miami, FL</h4>
+          <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </section>
   );
 }
--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -1,13 +1,58 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { SiGithub } from 'react-icons/si';
+import MyDP from '../../public/ritesh_dp.png';
+
 export function ProjectsSection() {
   return (
     <section id='projects' className='bg-green-400  my-4 p-8'>
-      <h3 className='text-6xl leading-24 font-semibold flex items-center after:border-b-2 after:grow after:ml-6'>Projects</h3>
-      <ul>
+      <h3 className='text-6xl leading-24 font-semibold flex items-center before:border-b-2 before:grow before:mr-6'>Projects</h3>
+      <ul className='grid grid-cols-3'>
         <li>
-          <p>card with pic, title, tech used list, and some description.</p>
-          <p>Also include code github</p>
-          <p>if has its own website, include it</p>
-          <p>when clicked on the card - should open a modal with detailed description</p>
+          <Card className='flex flex-col gap-4'>
+            {/* when clicked on the card - should open a modal with detailed description */}
+            <CardHeader>
+              <AspectRatio ratio={1.5} className='bg-muted rounded-lg'>
+                <Image src={MyDP} alt='project-image' fill className='h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale' />
+              </AspectRatio>
+            </CardHeader>
+            <CardContent>
+              <CardTitle>Project Title</CardTitle>
+              <CardDescription>
+                <p>
+                  Project Description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime deleniti omnis.
+                  <button className='inline'>... More</button>
+                </p>
+              </CardDescription>
+            </CardContent>
+            <CardFooter className='flex flex-col items-start gap-3'>
+              <div className='flex gap-2'>
+                <Button>
+                  <a href='#'>
+                    <div className='flex items-center gap-2'>
+                      <SiGithub /> Github
+                    </div>
+                  </a>
+                </Button>
+                <Button>
+                  <a href='#'>
+                    <div className='flex items-center gap-2'>
+                      <FaExternalLinkAlt /> Demo
+                    </div>
+                  </a>
+                </Button>
+              </div>
+              <div className='flex flex-wrap gap-2'>
+                <Badge variant='secondary'>Tool 1</Badge>
+                <Badge variant='secondary'>Tool 2</Badge>
+                <Badge variant='secondary'>Tool 3</Badge>
+              </div>
+            </CardFooter>
+          </Card>
         </li>
       </ul>
     </section>
